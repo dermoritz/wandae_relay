d4495a2ce16a5acf1e3b93566f4435c31ae75684
ta <tanteanni@hotmail.com>
2015-08-12 15:31:44 +0200

    init

---
+++ src/main/java/com/wandae/relay/rest/ClientApi.java
@@ -0,0 +1,8 @@
+package com.wandae.relay.rest;
+
+import javax.ws.rs.Path;
+
+@Path("/client")
+public class ClientApi {
+
+}
---
+++ src/main/java/com/wandae/relay/rest/RestEndpoint.java
@@ -0,0 +1,9 @@
+package com.wandae.relay.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestEndpoint extends Application {
+
+}
